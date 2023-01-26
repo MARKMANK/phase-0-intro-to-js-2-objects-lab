20de16a6ba4b7b1be281df8d99706fdd3b7114e7
kevinmarkman <kevin.w.markman@gmail.com>
2023-01-25 20:10:24 -0600

    Object Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: `Dory`,
+    streetAddress: `42 Wallaby Way`,
+};
+
+function updateEmployeeWithKeyAndValue(){
+    const updatedEmployee = {...employee};
+    updatedEmployee.name = `Sam`;
+    updatedEmployee.streetAddress = `11 Broadway`;
+    return updatedEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = `Sam`;
+    employee.streetAddress = `12 Broadway`;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(){
+    const updatedEmployee = {...employee};
+    delete updatedEmployee.name;
+    return updatedEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name;
+    delete employee.streetAddress;
+    return employee;
+};
